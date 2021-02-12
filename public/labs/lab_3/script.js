ae563b1c4bba710e8e9d0adc7571ea43bc197794
nisaputri <pnisa1911@gmail.com>
2021-02-11 23:47:20 -0500

    UPDATE - Update the lab with initial JavaScript functions

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,2 +1,30 @@
 /* Put your javascript in here */
 
+   //declaring new variables
+   let listOfPictures = document.querySelector('ul');
+   let list_images = document.getElementById("images").getElementsByTagName("li");
+   let image_width = 135;
+   let count_image = 1;
+   let position = 0; 
+
+   function onLoadOfPage() {
+     const prev = document.querySelector('#prev');
+     prev.addEventListener('click', (event) => {
+       position += image_width * count_image;
+       position = Math.min(position, 0)
+       listOfPictures.style.marginLeft = position + 'px';
+       console.log('clickprev');
+     });
+
+     const next = document.querySelector('#next');
+     next.addEventListener('click', (event) => {
+       position -= image_width * count_image;
+       position = Math.max(position, -image_width * (list_images.length - count_image));
+       listOfPictures.style.marginLeft = position + 'px';
+       console.log('clicknext');
+
+     });
+   }
+
+   window.onload = onLoadOfPage;
+
