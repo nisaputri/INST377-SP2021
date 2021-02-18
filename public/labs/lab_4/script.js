8fd71e0059cb436e05df508e7ffb8219fb120c3c
nisaputri <pnisa1911@gmail.com>
2021-02-17 20:29:20 -0500

    UPDATE - Updating the labs url for server and tidy up the index

--- public/labs/lab_4/script.js
+++
@@ -1,9 +0,0 @@
-const { expr } = require('cypress/types/jquery');
-
-function onLoad() {
-    console.log('script loaded');
-    let express = require('express')
-    let app = express()
-}
-
-window.onload = onLoad;
